6f2a5fdf1a66dc502f3bef080051189aec94788f
klymovy4 <roman.klymovych@receptor.ai>
2024-12-10 20:38:05 +0200

    second  commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ function App() {
       <div>
         <h4>Hello People meet =)</h4>
          <p>let start to do the best </p>
+         <p>test 1</p>
+
+         <p>Hello let's start</p>
       </div>
     </>
   )
