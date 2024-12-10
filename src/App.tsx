faf4f50453c42dabcd5bc871c1812e15a4e05614
klymovy4 <roman.klymovych@receptor.ai>
2024-12-10 19:35:12 +0200

    first commit  ypa

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h4>Hello People meet =)</h4>
+         <p>let start to do the best </p>
+      </div>
+    </>
+  )
+}
+
+export default App
