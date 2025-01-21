493afa702e90600ac93afe8a7ea048c6f4bb1ad7
magemore <alex@magemore.com>
2025-01-21 15:26:14 +0200

    server

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,36 @@
-// index.js
-const express = require('express');
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+
 require('dotenv').config();
+// Import the Express module
+const express = require('express');
+const path = require('path');
 
+// Create an instance of an Express app
 const app = express();
-app.use(express.json()); // Для обработки JSON
 
-// Подключение к MongoDB
-mongoose
-    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch((err) => console.log(err));
+// Define a port
+const PORT = 3000;
 
-// Модель пользователя
-const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-});
-const User = mongoose.model('User', UserSchema);
+const staticAssetsPath = '/usr/share/nginx/html/peoplemeetAWS/dist/assets';
+app.use('/assets', express.static(staticAssetsPath));
 
-// Регистрация
-app.post('/register', async (req, res) => {
-    const { username, password } = req.body;
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = new User({ username, password: hashedPassword });
-        await newUser.save();
-        res.status(201).json({ message: 'User registered!' });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+app.get('/test', (req, res) => {
+    res.send('Test');
 });
 
-// Запуск сервера
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Define a simple route
+app.get('*', (req, res) => {
+    //  res.send('Hello, World!');
+    res.sendFile('/usr/share/nginx/html/peoplemeetAWS/dist/index.html');
+});
+
+
+//app.get('/', (req, res) => {
+//res.send('Hello, World!');
+//    res.sendFile('/usr/share/nginx/html/peoplemeetAWS/dist/index.html');
+//});
+
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
