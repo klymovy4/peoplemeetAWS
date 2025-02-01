f034b203357c70f4438bdcbd15561aac728a0568
magemore <alex@magemore.com>
2025-02-01 17:12:53 +0200

    register_process

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import { Database } from "bun:sqlite";
 const db = new Database("/home/ec2-user/db/peoplemeet.db");
 db.run("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, name TEXT, email TEXT, password TEXT, age INTEGER, sex TEXT, description TEXT, image TEXT, lat REAL, lng REAL, is_online INTEGER)");
 
+// insert into users (name,email,age,sex,description,image,lat,lng, is_online) VALUES ('vasya','vasya@gmail.com',50,'male','just vasya', 'vasya.jpg',49.39898288068253,49.39898288068253,TRUE);
 
 // Create an instance of an Express app
 const app = express();
@@ -17,11 +18,10 @@ const PORT = 3000;
 const staticAssetsPath = '/usr/share/nginx/html/peoplemeetAWS/dist/assets';
 app.use('/assets', express.static(staticAssetsPath));
 
-app.get('/test', (req, res) => {
-    // res.send('Test');
-    db.run("INSERT INTO messages (text) VALUES ('Hello, World 100!')");
-    const row = db.query("SELECT text FROM messages").get();
-    res.send(row.text);
+app.get('/register_process', (req, res) => {
+    const { username, password } = req.body; // Extract data from POST request
+    console.log(`Received: ${username}, ${password}`);
+    res.json({ message: "User registered successfully", data: req.body });
 });
 
 app.get('/read', (req, res) => {
