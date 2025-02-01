f970ff8e00b6b2bca77fce101a3d842c905d78b9
magemore <alex@magemore.com>
2025-02-01 16:24:33 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 // Import the Express module
 const express = require('express');
 const path = require('path');
+import { Database } from "bun:sqlite";
+const db = new Database("/home/ec2-user/db/hello.db");
 
 // Create an instance of an Express app
 const app = express();
@@ -14,21 +16,20 @@ const staticAssetsPath = '/usr/share/nginx/html/peoplemeetAWS/dist/assets';
 app.use('/assets', express.static(staticAssetsPath));
 
 app.get('/test', (req, res) => {
-    res.send('Test');
+    // res.send('Test');
+    db.run("CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY, text TEXT)");
+    db.run("INSERT INTO messages (text) VALUES ('Hello, World!')");
+    const row = db.query("SELECT text FROM messages").get();
+    res.send(row.text); // Output: Hello, World!
 });
+
+
 
 // Define a simple route
 app.get('*', (req, res) => {
     //  res.send('Hello, World!');
     res.sendFile('/usr/share/nginx/html/peoplemeetAWS/dist/index.html');
 });
-
-
-//app.get('/', (req, res) => {
-//res.send('Hello, World!');
-//    res.sendFile('/usr/share/nginx/html/peoplemeetAWS/dist/index.html');
-//});
-
 
 // Start the server
 app.listen(PORT, () => {
