f16a8bf4ba73e5fd3e1b141a847ed61ecf546d35
magemore <alex@magemore.com>
2025-02-01 16:30:29 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use('/assets', express.static(staticAssetsPath));
 app.get('/test', (req, res) => {
     // res.send('Test');
     db.run("CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY, text TEXT)");
-    db.run("INSERT INTO messages (text) VALUES ('Hello, World!')");
+    db.run("INSERT INTO messages (text) VALUES ('Hello, World 2!')");
     const row = db.query("SELECT text FROM messages").get();
     res.send(row.text);
 });
