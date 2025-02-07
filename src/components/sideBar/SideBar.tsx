49220e5edd706d5f86e70b67c3660986ffbe8fef
klymovy4 <roman.klymovych@receptor.ai>
2025-02-08 01:03:05 +0200

    update version

--- src/components/sideBar/SideBar.tsx
+++ src/components/sideBar/SideBar.tsx
@@ -128,7 +128,7 @@ const SideBar = () => {
                    <ListItemIcon>
                       <SmsFailedIcon/>
                    </ListItemIcon>
-                   <ListItemText primary={'v: v0.0.27'}/>
+                   <ListItemText primary={'v: v0.0.30'}/>
                 </ListItemButton>
              </List>
           </Box>
