38ffba26dd56c7dd177d8d1246bf594030ec8df1
klymovy4 <roman.klymovych@receptor.ai>
2025-02-08 14:03:06 +0200

    create zoom for marker

--- src/components/sideBar/SideBar.tsx
+++ src/components/sideBar/SideBar.tsx
@@ -128,7 +128,7 @@ const SideBar = () => {
                    <ListItemIcon>
                       <SmsFailedIcon/>
                    </ListItemIcon>
-                   <ListItemText primary={'v: v0.0.33'}/>
+                   <ListItemText primary={'v: v0.0.34'}/>
                 </ListItemButton>
              </List>
           </Box>
--- src/pages/MapPage/Map.tsx
+++ src/pages/MapPage/Map.tsx
@@ -1,19 +1,19 @@
 import {MapContainer} from 'react-leaflet/MapContainer';
 import {TileLayer} from 'react-leaflet/TileLayer';
 import {useHeaderHeight} from "../../utils/hooks.ts";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import MarkerComponent from "../../components/marker/MarkerComponent.tsx";
 import mockedUsers from '../../mockedData/mockedUsers.json';
 import {useAppSelector} from "../../redux/hooks";
 import {useMap} from "react-leaflet/hooks";
 
-const MapUpdater = ({ lat, lng }: { lat: number; lng: number }) => {
+const MapUpdater = ({zoom, lat, lng}: { zoom: number, lat: number; lng: number }) => {
    const map = useMap();
 
    useEffect(() => {
       if (map && lat && lng) {
          console.log(`📌 Update map: lat=${lat}, lng=${lng}`);
-         map.setView([lat, lng], map.getZoom(), { animate: true });
+         map.setView([lat, lng], zoom, {animate: true});
       }
    }, [lat, lng, map]);
 
@@ -22,7 +22,8 @@ const MapUpdater = ({ lat, lng }: { lat: number; lng: number }) => {
 
 const Map = () => {
    const headerHeight = useHeaderHeight();
-   const {isOnline, location, name, image, description, age, sex } = useAppSelector(state => state.user);
+   const zoom = useRef<number>(17);
+   const {isOnline, location, name, image, description, age, sex} = useAppSelector(state => state.user);
    const [heightHeader, setHeightHeader] = useState<number>(0);
 
    const user = {
@@ -36,7 +37,8 @@ const Map = () => {
 
    return (
        <div style={{height: `calc(100svh - ${heightHeader}px)`}}>
-          <MapContainer center={[location.lat ?? -0.09, location.lng ?? 51.505]} zoom={17} scrollWheelZoom={true}
+          <MapContainer center={[location.lat ?? -0.09, location.lng ?? 51.505]} zoom={zoom.current}
+                        scrollWheelZoom={true}
                         style={{height: "100%", width: "100%"}}>
              <TileLayer
                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -45,8 +47,8 @@ const Map = () => {
              {isOnline && mockedUsers.map(user => {
                 return <MarkerComponent user={user} key={user.id}/>;
              })}
-             <MarkerComponent user={user} self={true} />
-             <MapUpdater lat={location.lat ?? -0.09} lng={location.lng ?? 51.505} />
+             <MarkerComponent user={user} self={true}/>
+             <MapUpdater lat={location.lat ?? -0.09} lng={location.lng ?? 51.505} zoom={zoom.current}/>
           </MapContainer>
        </div>
    )
