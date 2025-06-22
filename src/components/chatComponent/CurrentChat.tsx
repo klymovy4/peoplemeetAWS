a978f64d8f6959ce6c7cbf3e0423907b63de666f
klymovy4 <roman.klymovych@receptor.ai>
2025-06-22 22:26:38 +0300

    fix border

--- src/components/chatComponent/CurrentChat.tsx
+++ src/components/chatComponent/CurrentChat.tsx
@@ -83,7 +83,6 @@ const CurrentChat = () => {
               width: "100%",
               height: "100%",
               padding: 0,
-              border: '1px solid red'
            }}
        >
           <Box sx={{
